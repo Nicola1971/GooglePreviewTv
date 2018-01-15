84d090eec36234460ba436dacfed5c877fc7d696
Nicola <modxit@modx.it>
2018-01-15 12:16:45 +0100

    title leght and posttitle
    fix for #1 and #3

--- assets/tvs/googlepreview/googlepreview.customtv.php
+++ assets/tvs/googlepreview/googlepreview.customtv.php
@@ -1,7 +1,7 @@
 <?php
 /*
  GooglePreviewTV — custom TV for SEo4Evo
- Version 14.01.18 by Nicola Lambathakis, http://www.tattoocms.it
+ Version 15.01.18 by Nicola Lambathakis, http://www.tattoocms.it
 */
 
 if (IN_MANAGER_MODE != 'true') {
@@ -12,19 +12,30 @@ global $modx,$_lang;
 $id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
 $value = empty($row['value']) ? $row['default_text'] : $row['value'];
 $rid = $row['id'];
+$site_name = $modx->config['site_name'];
 
-//title
+// Manual configuration of Seo4Evo params
+$MetaDescriptionTv = isset($MetaDescriptionTv) ? $MetaDescriptionTv : 'MetaDescription';
+//$preTitle = "$site_name | ";
+$postTitle = " | $site_name";
+
+//Get the title
 $pagetitle = $modx->documentObject['pagetitle'];
 $CTitle = $modx->getTemplateVarOutput('CustomTitle',$id);
 $Custom = $CTitle['CustomTitle'];
-
+//check custoom title tv
 if(!$Custom == ""){
 $MetaTitle = "$preTitle$Custom$postTitle";
 } else {
       $MetaTitle = "$preTitle$pagetitle$postTitle";
    }
-// *** Meta Description ***
-$MetaDescriptionTv = isset($MetaDescriptionTv) ? $MetaDescriptionTv : 'MetaDescription';
+$max_length = '70';
+if (strlen($MetaTitle) > $max_length) {
+$Title = substr($MetaTitle, 0, $max_length);
+}
+else {$Title = $MetaTitle;}
+
+// Get Meta Description ***
 $dyndesc = $modx->runSnippet(
         "DynamicDescription",
         array(
@@ -32,9 +43,9 @@ $dyndesc = $modx->runSnippet(
 			"maxWordCount=" => "25"
         )
 );
-//url
+//Get the url
 $url = $modx->makeUrl($id, '', '', 'full');
-
+//output Preview Tv
 $output .="
 <div>
 <input type='text' style=\"display:none;\"  id=\"tv$rid\" name=\"tv$rid\" value=\"tv$rid\" />
@@ -46,7 +57,7 @@ $output .="
 .s-desc{font-size:13px;}
 </style>
 <div class=\"s-google\">
-  <span class=\"s-titl\">$MetaTitle</span>
+  <span class=\"s-titl\">$Title</span>
   <span class=\"s-url\">$url</span>
   <span class=\"s-desc\">$dyndesc.</span>
 </div>
